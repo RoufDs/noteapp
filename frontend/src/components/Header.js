fa88cbf1d9d61a5874db3a8295419de2d8f5235d
RoufDs <four.3687zero@gmail.com>
2022-09-08 12:04:08 +0700

    Proxy URL

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            Our Header
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/pages/NoteListPage.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import ListItem from '../components/ListItem'
+
+const NoteListPage = () => {
+
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        let response = await fetch('/api/notes/')
+        let data = await response.json()
+        console.log("DATA:", data)
+        setNotes(data)
+    }
+
+    return (
+        <div>
+            <div className='notes-list'>
+                {notes.map((note, index) => (
+                    <ListItem key={index} note={note} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default NoteListPage
